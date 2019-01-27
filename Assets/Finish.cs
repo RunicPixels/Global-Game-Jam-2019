2c3de38481552599e1ae351c12b84225df310a64
Cheezegami <superronald@live.nl>
2019-01-27 14:29:12 +0100

    finish screen

--- Assets/Finish.cs
+++ Assets/Finish.cs
@@ -10,6 +10,10 @@ public class Finish : MonoBehaviour
 
     private bool finish;
     private float opacity;
+    private float duration = 5;
+    private float realCount;
+    private bool truefinish = false;
+    private float fadeDuration = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +23,24 @@ public class Finish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fadeToBlack.color = new Color(0, 0, 0, opacity);
+        if(finish == true) {
+            realCount += Time.deltaTime;
+            if(realCount > duration) {
+                truefinish = true;
+            }
+            
+        }
+
+        if(truefinish == true) {
+            opacity += Time.deltaTime / fadeDuration;
+        }
+
+        if(opacity > 1) {
+            SceneManager.LoadScene(0);
+        }
     }
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.tag == "Player") {
             finish = true;
