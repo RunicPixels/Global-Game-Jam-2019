4022b0072fd338706b6f2c96cebdc79c1f43a96b
Cheezegami <superronald@live.nl>
2019-01-27 13:48:12 +0100

    laatste versie

---
+++ Assets/Bed.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : MonoBehaviour
+{
+    public SpriteRenderer light;
+    bool doLight = false;
+    float opacity = 0;
+    float duration = 1;
+    bool fadingOut = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doLight == true) {
+            if (fadingOut) {
+                opacity -= duration * Time.deltaTime;
+                if (opacity < 0) {
+                    doLight = false;
+                }
+            }
+            else {
+                opacity += duration * Time.deltaTime;
+                if (opacity > 1) {
+                    fadingOut = true;
+                }
+            }
+            light.color = new Color(1, 1, 1, opacity);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+
+            doLight = true;
+            fadingOut = false;
+        }
+    }
+
+
+}
---
+++ Assets/Lamp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lamp : MonoBehaviour
+{
+    private SpriteRenderer image;
+    public Sprite active, inactive;
+    public GameObject light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == "Player") {
+            image.sprite = active;
+            light.SetActive(true);
+
+        }
+    }
+}
